fb4c90b5420777de372bb14a44bf65b16ca97103
Sergio H <sergiohernandezroman@gmail.com>
2022-05-31 20:30:29 +0200

    add uppgrade form tsx

--- src/form.tsx
+++ src/form.tsx
@@ -1,44 +1,89 @@
-import React from 'react';
-export function Form() {
-  const data = {};
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 
-  return (
-    <>
-      <div className="container">
-        <form action="">
-          <div>
-            <label htmlFor="name"></label>
-            <input type="text" name="name" placeholder="Name" required />
-          </div>
-          <div>
-            <label htmlFor="lastName"></label>
-            <input type="text" name="lastName" placeholder="Last name" />
-          </div>
-          <div>
-            <label htmlFor="birthday"></label>
-            <input type="date" name="birthday" placeholder="Year of birth" />
-          </div>
-          <div>
-            <label htmlFor="">Male</label>
-            <input type="radio" name="gender" id="" />
-            <label htmlFor="">Female</label>
-            <input type="radio" name="gender" id="" />
-            <label htmlFor="">Other</label>
-            <input type="radio" name="gender" id="" />
-            <label htmlFor="">prefer not to mention</label>
-            <input type="radio" name="gender" id="" />
-          </div>
-          <div>
-            <input type="email" name="" placeholder="Email" />
-          </div>
-          <div>
-            <label htmlFor="newsLet">
-              Desea recibir información de nuestra newsletter?
-            </label>
-            <input type="checkbox" name="newsLet" id="" />
-          </div>
-        </form>
-      </div>
-    </>
+export function Form() {
+  const [formData, setFormData] = useState({
+    name: '',
+    surname: '',
+    birthday: '',
+    gender: '',
+    email: '',
+    newsletter: false,
+  });
+  function handleSubmit(ev: FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+    console.log('Guardando', formData);
+  }
+
+  function handleChange(ev: ChangeEvent) {
+    const eventTarget = ev.target as HTMLFormElement;
+    const value =
+      eventTarget.type === 'checkbox' ? eventTarget.checked : eventTarget.value;
+    setFormData({ ...formData, [eventTarget.name]: value });
+  }
+  const form1 = (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name"></label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="surname"></label>
+          <input
+            type="text"
+            name="surname"
+            placeholder="Surname"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="birthday"></label>
+          <input
+            type="date"
+            name="birthday"
+            placeholder="Year of birth"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Male</label>
+          <input type="radio" name="gender" id="male" value="male" />
+          <label htmlFor="">Female</label>
+          <input type="radio" name="gender" id="female" value="female" />
+          <label htmlFor="">Other</label>
+          <input type="radio" name="gender" id="other" value="other" />
+          <label htmlFor="">prefer not to mention</label>
+          <input type="radio" name="gender" id="prefer" value="prefer" />
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="newsletter">
+            Desea recibir información de nuestra newsletter?
+          </label>
+          <input
+            type="checkbox"
+            name="newsletter"
+            id=""
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Guardar</button>
+      </form>
+    </div>
   );
+  //   const form2 = <div className="container"></div>;
+
+  return form1;
 }
